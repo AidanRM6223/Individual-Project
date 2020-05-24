5d9dbe86c894e76e431864af0c19a69328264a68
AidanRM6223 <63170558+AidanRM6223@users.noreply.github.com>
2020-05-24 11:08:07 -0700

    Update

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-
 var loadTime;
 function loadPage() {
-  loadTime = setTimeout(showPage, 3000);
+  loadTime = setTimeout(showPage, 2000);
 }
 function showPage() {
   document.getElementById("loader").style.display = "none";
-  document.getElementById("home").style.visibility = "visible";
+  document.getElementById("main").style.display = "block";
+}
+
+var slideIndex = [1,1];
+/* Class the members of each slideshow group with different CSS classes */
+var slideId = ["slides1", "slides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no]-1].style.display = "block";
 }
