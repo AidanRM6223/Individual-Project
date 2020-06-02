6b186d0b2d799d66138b1a1a87373a8a44419c68
AidanRM6223 <63170558+AidanRM6223@users.noreply.github.com>
2020-06-01 18:59:16 -0700

    Temporarily removed accordions

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function showPage() {
 }
 
 var slideIndex = [1,1];
-/* Class the members of each slideshow group with different CSS classes */
 var slideId = ["slides1", "slides2"]
 showSlides(1, 0);
 showSlides(1, 1);
@@ -20,10 +19,38 @@ function plusSlides(n, no) {
 function showSlides(n, no) {
   var i;
   var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}
+  if (n > x.length) {slideIndex[no] = 1}    
   if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+     x[i].style.display = "none";  
   }
-  x[slideIndex[no]-1].style.display = "block";
+  x[slideIndex[no]-1].style.display = "block";  
+}
+
+var acc = document.getElementsByClassName("accordion");
+var j;
+
+for (j = 0; j < acc.length; j++) {
+  acc[j].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("accordion-active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+function topNav() {
+  var x = document.getElementById("topnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
