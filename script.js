27652c6ef5702439b099c0f9457ccf2ddec4249e
AidanRM6223 <63170558+AidanRM6223@users.noreply.github.com>
2020-06-02 19:49:31 -0700

    Updated Accordions for basic functionality

--- script.js
+++ script.js
@@ -27,25 +27,6 @@ function showSlides(n, no) {
   x[slideIndex[no]-1].style.display = "block";  
 }
 
-var acc = document.getElementsByClassName("accordion");
-var j;
-
-for (j = 0; j < acc.length; j++) {
-  acc[j].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("accordion-active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
 function topNav() {
   var x = document.getElementById("topnav");
   if (x.className === "topnav") {
